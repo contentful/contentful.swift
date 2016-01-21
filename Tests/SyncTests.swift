1e3a331b262d08a4c99c2b1a469a984ce6ed4973
Boris Bügling <boris@icculus.org>
2016-01-21 15:51:05 +0100

    Add tests for syncing specific resources

--- Tests/SyncTests.swift
+++ Tests/SyncTests.swift
@@ -12,21 +12,25 @@ import Nimble
 import Quick
 
 class SyncTests: ContentfulBaseTests {
+    func waitUntilSyncMatching(matching: [String:AnyObject], action: (space: SyncSpace) -> ()) {
+        waitUntil { done in
+            self.client.initialSync(matching).1.next {
+                action(space: $0)
+                done()
+            }.error {
+                fail("\($0)")
+                done()
+            }
+        }
+    }
+
     override func spec() {
         super.spec()
 
         it("can perform an initial sync for a space") {
-            waitUntil { done in
-                self.client.initialSync().1.next {
-                    expect($0.assets.count).to(equal(4))
-                    expect($0.entries.count).to(equal(11))
-
-                    done()
-                }.error {
-                    fail("\($0)")
-
-                    done()
-                }
+            self.waitUntilSyncMatching([String:AnyObject]()) {
+                expect($0.assets.count).to(equal(4))
+                expect($0.entries.count).to(equal(11))
             }
         }
 
@@ -44,6 +48,20 @@ class SyncTests: ContentfulBaseTests {
 
                     done()
                 }
+            }
+        }
+
+        it("can sync data of a specific type") {
+            self.waitUntilSyncMatching(["type": "Asset"]) {
+                expect($0.assets.count).to(equal(4))
+                expect($0.entries.count).to(equal(0))
+            }
+        }
+
+        it("can sync entries of a specific content type") {
+            self.waitUntilSyncMatching(["type": "Entry", "content_type": "cat"]) {
+                expect($0.assets.count).to(equal(0))
+                expect($0.entries.count).to(equal(3))
             }
         }
     }
