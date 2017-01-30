abe2c781db19c589ed808f4e598ce87da6008174
JP Wright <jp@contentful.com>
2017-01-30 12:19:22 +0100

    Update Package.swift dependencies and exclude test files

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Contentful",
     dependencies: [
-        .Package(url: "https://github.com/neonichu/Decodable", majorVersion: 0),
-        .Package(url: "https://github.com/jensravens/Interstellar", majorVersion: 1),
-        .Package(url: "https://github.com/neonichu/Clock", majorVersion: 0),
-    ]
+        .Package(url: "https://github.com/anviking/Decodable", majorVersion: 0),
+        .Package(url: "https://github.com/jensravens/Interstellar", majorVersion: 2),
+    ],
+    exclude: ["Tests/"]
 )
