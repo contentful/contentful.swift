355c94f0d537657496ce2e7df45a55892d5d7517
JP Wright <jp@contentful.com>
2017-02-16 14:22:36 +0100

    Debug pre-push hook to handle stashes properly

--- pre-push.sh
+++ pre-push.sh
@@ -6,15 +6,36 @@
 # to commit without pre-commit hook run "git commit --no-verify"
 
 # Inspired by http://codeinthehole.com/writing/tips-for-using-a-git-pre-commit-hook/
+# but with some modifications
 
 
+# http://stackoverflow.com/a/20480591/4068264 how to properly stash/unstash
+# First, stash index and work dir, keeping only the
+# to-be-committed changes in the working directory.
+old_stash=$(git rev-parse -q --verify refs/stash)
+git stash save -q --keep-index --include-untracked
+new_stash=$(git rev-parse -q --verify refs/stash)
 
-# Stash unstaged changes before testing so we can isolate the broken tests to commits
-git stash -q --keep-index
+# If there were no changes (e.g., `--amend` or `--allow-empty`)
+# then nothing was stashed, and we should skip everything,
+# including the tests themselves.  (Presumably the tests passed
+# on the previous commit, so there is no need to re-run them.)
+if [ "$old_stash" = "$new_stash" ]; then
+    echo "pre-push script: No changes to test"
+    exit 0
+fi
 
 # Run tests
 make test
 RESULT=$?
-git stash pop -q
+
+# Restore changes
+git reset --hard -q && git stash apply --index -q && git stash drop -q
+
+# Exit with status from test-run: nonzero prevents commit
+exit $status
+
+
+git stash apply stash^{/$PRETEST}
 [ $RESULT -ne 0 ] && exit 1
 
