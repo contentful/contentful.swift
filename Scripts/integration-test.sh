78dff527a13972c41433bcd4adb4158281a8676c
JP Wright <jp@contentful.com>
2017-02-23 13:45:44 +0100

    Trigger api-coverage-reporter build on successful travis exit

---
+++ Scripts/integration-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Run Swift specific api-coverage tests
+
+ENV='"REPO_SDK=swift REPO_SLUG='$TRAVIS_REPO_SLUG' REPO_COMMIT='$TRAVIS_COMMIT'"'
+
+BODY="{
+  \"request\": {
+    \"message\": \"$TRAVIS_REPO_SLUG SDK Triggered Request\",
+    \"branch\":\"travis_experiments\",
+    \"config\": {\"env\": $ENV}
+  }
+}"
+
+# api-coverage
+curl -s -X POST \
+	-d "$BODY" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json"   \
+    -H "Travis-API-Version: 3"   \
+    -H "Authorization: token $TRAVIS_TOKEN" \
+    'https://api.travis-ci.com/repo/1336919/requests'
+
