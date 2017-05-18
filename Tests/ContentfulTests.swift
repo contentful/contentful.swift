309008b1bfd079e8da55f034012fbfe7eb2c722a
JP Wright <jp@contentful.com>
2017-05-18 13:35:12 +0200

    Use regular expressions to validate version numbers in tests

--- Tests/ContentfulTests.swift
+++ Tests/ContentfulTests.swift
@@ -41,7 +41,25 @@ class ClientConfigurationTests: XCTestCase {
         let clientConfiguration = ClientConfiguration.default
         let userAgentString = clientConfiguration.userAgentString
 
-        expect(userAgentString).to(equal("sdk contentful.swift/0.4.0; platform Swift/3.1; os iOS/\(osVersionString);"))
+        let onlyVersionNumberRegexString = "\\d+\\.\\d+\\.\\d+(-(beta|RC|alpha)\\d*)?"
+        let versionMatchingRegexString = onlyVersionNumberRegexString + "$"
+        let versionMatchingRegex = try! NSRegularExpression(pattern: versionMatchingRegexString, options: [])
+        // First test the regex itself
+        for validVersionString in ["0.10.0", "10.3.2-RC", "10.2.0-beta1", "0.4.79-alpha"] {
+            // expect 1 matc
+            let matches = versionMatchingRegex.matches(in: validVersionString, options: [], range: NSRange(location: 0, length: validVersionString.characters.count))
+            expect(matches.count).to(equal(1))
+        }
+
+        for invalidVersionString in ["0..9","0.a.9", "9.1", "0.10.9-", "0.10.9-ri", "0.10.9-RCHU"] {
+            // expect 0 matches
+            let matches = versionMatchingRegex.matches(in: invalidVersionString, options: [], range: NSRange(location: 0, length: invalidVersionString.characters.count))
+            expect(matches.count).to(equal(0))
+        }
+
+        let regex = try! NSRegularExpression(pattern: "sdk contentful.swift/\(onlyVersionNumberRegexString); platform Swift/3.1; os iOS/\(osVersionString);" , options: [])
+        let matches = regex.matches(in: userAgentString, options: [], range: NSRange(location: 0, length: userAgentString.characters.count))
+        expect(matches.count).to(equal(1))
 
         let client = Client(spaceId: "", accessToken: "", clientConfiguration: clientConfiguration)
         expect(client.urlSession.configuration.httpAdditionalHeaders?["X-Contentful-User-Agent"]).toNot(beNil())
