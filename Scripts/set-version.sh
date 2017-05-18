1f53f4535bd66ea84bf454d33917ffdb7fb22174
JP Wright <jp@contentful.com>
2017-05-18 13:35:12 +0200

    Use regular expressions to validate version numbers in tests

--- Scripts/set-version.sh
+++ Scripts/set-version.sh
@@ -6,4 +6,3 @@ echo "CONTENTFUL_SDK_VERSION=$1" > .env
 echo "export CONTENFUL_SDK_VERSION=$1" > .envrc
 direnv allow
 
-echo "Done setting new version number. Don't forget to update expecations in user agent string tests! See `'testUserAgentString()'`"
