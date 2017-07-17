9894e663cac64e855a475b7bc9c3d37c810e7fda
JP Wright <jp@contentful.com>
2017-07-17 22:33:27 +0200

    Send entire SyncSpace to persistence integration
    Prune dead code and minor refactors

--- Sources/Persistence.swift
+++ Sources/Persistence.swift
@@ -18,7 +18,19 @@ import Foundation
 public protocol PersistenceIntegration: Integration {
 
     /**
+     Update the local datastore with the latest delta messages from the most recently fetched SyncSpace response.
+     
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
+     */
+    func update(with syncSpace: SyncSpace)
+
+
+    /**
      This is called whenever a new Asset was created or an existing one was updated.
+
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
 
      - parameter asset: The created/updated Asset
      */
@@ -27,6 +39,9 @@ public protocol PersistenceIntegration: Integration {
     /**
      This is called whenever an Asset was deleted.
 
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
+
      - parameter assetId: Identifier of the Asset that was deleted.
      */
     func delete(assetWithId: String)
@@ -34,12 +49,18 @@ public protocol PersistenceIntegration: Integration {
     /**
      This is called whenever a new Entry was created or an existing one was updated.
 
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
+
      - parameter entry: The created/updated Entry
      */
     func create(entry: Entry)
 
     /**
      This is called whenever an Entry was deleted.
+
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
 
      - parameter entryId: Identifier of the Entry that was deleted.
      */
@@ -49,6 +70,9 @@ public protocol PersistenceIntegration: Integration {
      This method is called on completion of a successful `Client.initialSync` and `Client.nextSync`
      calls so that the sync token can be cached and future launches of your application can synchronize
      without doing an `initialSync`.
+     
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
      */
     func update(syncToken: String)
 
@@ -56,12 +80,18 @@ public protocol PersistenceIntegration: Integration {
      This method is called after all `Entry`s have been created and all links have been resolved.
      The implementation should map `Entry` fields that are `Link`s to persistent relationships in
      the underlying persistent data store.
+
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
      */
     func resolveRelationships()
 
     /**
      This method is called after all `Asset`s and `Entry`s have been tranformed to persistable data
      structures. The implementation should actually perform the save operation to the persisent database.
+     
+     There is no guarantee which thread this will be called from, so it is your responsibility when implementing
+     this method, to execute on whatever thread your local datastore may require operations to be executed on.
      */
     func save()
 }
