ef84349444da1edfe20bab99a1100e7d09174aad
JP Wright <jp@contentful.com>
2017-08-31 18:08:06 +0200

    Upgrade Interstellar and Nimble for Xcode 9 compatible versions

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Contentful",
     dependencies: [
         .Package(url: "https://github.com/Hearst-DD/ObjectMapper", majorVersion: 2, minor: 2),
-        .Package(url: "https://github.com/jensravens/Interstellar", majorVersion: 2)
+        .Package(url: "https://github.com/jensravens/Interstellar", majorVersion: 2, minor: 1)
     ],
     exclude: ["Tests/"]
 )
