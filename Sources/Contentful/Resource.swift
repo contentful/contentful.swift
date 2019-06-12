cd6c708ef2817f649f4892a740c52bcce602d93d
Manuel Maly <manuel.maly@gmail.com>
2019-06-12 14:02:57 +0200

    Fix redundant visibility modifiers that produce warnings in Xcode 10.2
    Remove empty attached value from SDKError.previewAPIDoesNotSupportSync
    Switch from hashValue to hash(into:) for Hashable conformance

--- Sources/Contentful/Resource.swift
+++ Sources/Contentful/Resource.swift
@@ -31,23 +31,23 @@ public protocol FlatResource {
 }
 
 public extension FlatResource where Self: Resource {
-    public var id: String {
+    var id: String {
         return sys.id
     }
 
-    public var type: String {
+    var type: String {
         return sys.type
     }
 
-    public var updatedAt: Date? {
+    var updatedAt: Date? {
         return sys.updatedAt
     }
 
-    public var createdAt: Date? {
+    var createdAt: Date? {
         return sys.createdAt
     }
 
-    public var localeCode: String? {
+    var localeCode: String? {
         return sys.locale
     }
 }
@@ -168,12 +168,15 @@ public class LocalizableResource: Resource, FlatResource, Decodable {
 
 extension LocalizableResource: Hashable {
 
-    public var hashValue: Int {
-        return id.hashValue
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(sys.updatedAt)
     }
 }
 
-/// Equatable implementation for `LocalizableResource`
-public func == (lhs: LocalizableResource, rhs: LocalizableResource) -> Bool {
-    return lhs.id == rhs.id && lhs.sys.updatedAt == rhs.sys.updatedAt
+extension LocalizableResource: Equatable {
+
+    public static func == (lhs: LocalizableResource, rhs: LocalizableResource) -> Bool {
+        return lhs.id == rhs.id && lhs.sys.updatedAt == rhs.sys.updatedAt
+    }
 }
