f900384cf1bc2db354c08e3617ebadff7466b432
Tomasz Szulc <mail@szulctomasz.com>
2020-09-26 13:37:17 +0200

    Cleanup LinkResolver

--- Sources/Contentful/LinkResolver/LinkResolver.swift
+++ Sources/Contentful/LinkResolver/LinkResolver.swift
@@ -7,42 +7,42 @@
 
 import Foundation
 
-class LinkResolver {
+internal class LinkResolver {
 
-    private var dataCache: DataCache = DataCache()
+    private enum Constant {
+        static let linksArrayPrefix = "linksArrayPrefix"
+    }
 
+    private var dataCache = DataCache()
     private var callbacks: [String: [(AnyObject) -> Void]] = [:]
-
-    private static let linksArrayPrefix = "linksArrayPrefix"
 
     /// Perform data decoding in the `decoding` block. The link resolver will call completion block
     /// when all the links are resolved.
-    internal func perform(decoding: @escaping () -> Void, completion: @escaping () -> Void) {
+    internal func perform(
+        decoding: @escaping () -> Void,
+        completion: @escaping () -> Void
+    ) {
         decoding()
         churnLinks(completion: completion)
     }
 
-    func cache(assets: [Asset]) {
-        for asset in assets {
-            dataCache.add(asset: asset)
-        }
+    internal func cache(assets: [Asset]) {
+        assets.forEach { dataCache.add(asset: $0) }
     }
 
-    func cache(entryDecodables: [EntryDecodable]) {
-        for entryDecodable in entryDecodables {
-            dataCache.add(entry: entryDecodable)
-        }
+    internal func cache(entryDecodables: [EntryDecodable]) {
+        entryDecodables.forEach { dataCache.add(entry: $0) }
     }
 
     // Caches the callback to resolve the relationship represented by a Link at a later time.
-    func resolve(_ link: Link, callback: @escaping (AnyObject) -> Void) {
+    internal func resolve(_ link: Link, callback: @escaping (AnyObject) -> Void) {
         let key = DataCache.cacheKey(for: link)
         // Swift 4 API enables setting a default value, if none exists for the given key.
         callbacks[key, default: []] += [callback]
     }
 
-    func resolve(_ links: [Link], callback: @escaping (AnyObject) -> Void) {
-        let linksIdentifier: String = links.reduce(into: LinkResolver.linksArrayPrefix) { id, link in
+    internal func resolve(_ links: [Link], callback: @escaping (AnyObject) -> Void) {
+        let linksIdentifier: String = links.reduce(into: Constant.linksArrayPrefix) { id, link in
             id += "," + DataCache.cacheKey(for: link)
         }
         callbacks[linksIdentifier, default: []] += [callback]
@@ -52,8 +52,8 @@ class LinkResolver {
     // where resources are cached before being resolved.
     private func churnLinks(completion: @escaping () -> Void) {
         for (linkKey, callbacksList) in callbacks {
-            if linkKey.hasPrefix(LinkResolver.linksArrayPrefix) {
-                let firstKeyIndex = linkKey.index(linkKey.startIndex, offsetBy: LinkResolver.linksArrayPrefix.count)
+            if linkKey.hasPrefix(Constant.linksArrayPrefix) {
+                let firstKeyIndex = linkKey.index(linkKey.startIndex, offsetBy: Constant.linksArrayPrefix.count)
                 let onlyKeysString = linkKey[firstKeyIndex ..< linkKey.endIndex]
                 // Split creates a [Substring] array, but we need [String] to index the cache
                 let keys = onlyKeysString.split(separator: ",").map { String($0) }
