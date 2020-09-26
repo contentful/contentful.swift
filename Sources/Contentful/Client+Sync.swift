b60750b383612bfc2f68026b52bf307dfd951f2f
Tomasz Szulc <mail@szulctomasz.com>
2020-09-26 17:45:04 +0200

    Update Client+Sync

--- Sources/Contentful/Client+Sync.swift
+++ Sources/Contentful/Client+Sync.swift
@@ -1,31 +1,31 @@
 //
-//  Client+Sync.swift
 //  Contentful
 //
 //  Created by Tomasz Szulc on 26/09/2020.
 //  Copyright © 2020 Contentful GmbH. All rights reserved.
 //
 
-import Foundation
-
 extension Client {
 
-    /// Performs a synchronization operation, updating the passed in `SyncSpace` object with
-    /// latest content from Contentful. If the passed in `SyncSpace` is a new empty instance with an empty
-    /// sync token, a full synchronization will be done.
-    /// Calling this will mutate passed in `SyncSpace `and also pass back  a reference to it in the completion
-    /// handler in order to allow chaining of operations.
-    ///
-    /// - Parameters:
-    ///   - syncSpace: The relevant `SyncSpace` to perform the subsequent sync on. Defaults to a new empty instance of `SyncSpace`.
-    ///   - syncableTypes: The types that can be synchronized.
-    ///   - completion: A handler which will be called on completion of the operation
-    /// - Returns: Returns the `URLSessionDataTask` of the request which can be used for request cancellation.
-    @discardableResult
-    public func sync(for syncSpace: SyncSpace = SyncSpace(),
-                     syncableTypes: SyncSpace.SyncableTypes = .all,
-                     then completion: @escaping ResultsHandler<SyncSpace>) -> URLSessionDataTask? {
+    /**
+    Performs a synchronization operation, updating the passed in `SyncSpace` instance with latest content from the server.
 
+    If passed in `SyncSpace` is an instance with empty sync token, full synchronization will be done.
+
+    Calling this will mutate passed in `SyncSpace `and also pass back  a reference to it in the completion handler
+    in order to allow chaining of operations.
+
+    - Parameters:
+        - syncSpace: Instance to perform subsqeuent sync on. Empty instance by default.
+        - syncableTypes: The types that can be synchronized.
+        - completion: The completion handler to call when the operation is complete.
+     */
+    @discardableResult
+    public func sync(
+        for syncSpace: SyncSpace = SyncSpace(),
+        syncableTypes: SyncSpace.SyncableTypes = .all,
+        then completion: @escaping ResultsHandler<SyncSpace>
+    ) -> URLSessionDataTask? {
         // Preview mode only supports `initialSync` not `nextSync`. The only reason `nextSync` should
         // be called while in preview mode, is internally by the SDK to finish a multiple page sync.
         // We are doing a multi page sync only when syncSpace.hasMorePages is true.
