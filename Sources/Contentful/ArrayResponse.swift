7adee7b477f2701bdd74413197ce5011d5d7c274
Guillermo Moraleda <guillermo.moraleda@external.contentful.com>
2024-11-19 09:10:05 +0100

    cleanup

--- Sources/Contentful/ArrayResponse.swift
+++ Sources/Contentful/ArrayResponse.swift
@@ -147,7 +147,7 @@ extension HomogeneousArrayResponse: Decodable {
                     """
                     throw SDKError.unparseableJSON(data: nil, errorMessage: errorMessage)
                 }
-                let entryDecodable = try entryDecodableType.popEntryDecodable(from: &entriesJSONContainer)
+                let entryDecodable = try entriesJSONContainer.decode(entryDecodableType)
                 entries.append(entryDecodable)
             }
 
@@ -331,7 +331,7 @@ extension KeyedDecodingContainer {
             // For includes, if the type of this entry isn't defined by the user, we skip serialization.
             if let type = contentTypes[contentTypeInfo.id] {
                 do {
-                    let entryModellable = try type.popEntryDecodable(from: &entriesJSONContainer)
+                    let entryModellable = try entriesJSONContainer.decode(type)
                     entries.append(entryModellable)
                 } catch {
                     if throwIfNotPresent {
